2a4db81a873e1bfaea3cd8dd775cc3ede0989c13
thepaeth <theera_w1@hotmail.com>
2023-01-04 21:59:42 +0700

    add init create table expenses if not exists table

---
+++ pkg/expenses/db.go
@@ -0,0 +1,33 @@
+package expenses
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+func InitDB() {
+	var err error
+	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal("Connect to database error", err)
+	}
+
+	createTB := `
+	CREATE TABLE IF NOT EXISTS expenses (
+		id SERIAL PRIMARY KEY,
+		title TEXT,
+		amount FLOAT,
+		note TEXT,
+		tags TEXT[]
+	);
+	`
+	_, err = db.Exec(createTB)
+	if err != nil {
+		log.Fatal("Can't create table", err)
+	}
+}
