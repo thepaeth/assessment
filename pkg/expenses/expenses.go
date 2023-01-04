54ec601f0fa36b0396eb9913f1e76a05a84e52d4
thepaeth <theera_w1@hotmail.com>
2023-01-05 01:39:43 +0700

    story: EXP01 create expenses

--- pkg/expenses/expenses.go
+++ pkg/expenses/expenses.go
@@ -1,13 +1,34 @@
 package expenses
 
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/lib/pq"
+)
+
 type Expenses struct {
-	ID int `json:"id"`
-	Title string `json:"title"`
-	Amount float64 `json:"amount"`
-	Note string `json:"note"`
-	Tags []string `json:"tags"`
+	ID     int      `json:"id"`
+	Title  string   `json:"title"`
+	Amount float64  `json:"amount"`
+	Note   string   `json:"note"`
+	Tags   []string `json:"tags"`
 }
 
 type Err struct {
 	Message string `json:"message"`
+}
+
+func CreateExpense(c echo.Context) error {
+	var exp Expenses
+	err := c.Bind(&exp)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, Err{Message: "Bad Request!!!"})
+	}
+	row := db.QueryRow("INSERT INTO expenses (title, amount, note, tags) values($1, $2, $3, $4) RETURNING id", exp.Title, exp.Amount, exp.Note, pq.Array(exp.Tags))
+	err = row.Scan(&exp.ID)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "Can't Create Expense!!!!"})
+	}
+	return c.JSON(http.StatusCreated, exp)
 }
--- server.go
+++ server.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/thepaeth/assessment/pkg/expenses"
 )
 
 func main() {
-	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
+	expenses.InitDB()
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.POST("/expenses", expenses.CreateExpense)
+
+	log.Fatal(e.Start(os.Getenv("PORT")))
+
+	// fmt.Println("Please use server.go for main file")
+	// fmt.Println("start at port:", os.Getenv("PORT"))
 }
