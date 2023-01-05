7bb0b9987df23f0c1725435201da19337bdbe1ad
Thepaeth <theera.cpe12@gmail.com>
2023-01-05 16:09:15 +0700

    rename file expenses_test.go to expenses_integration_test.go

---
+++ pkg/expenses/expenses_integretion_test.go
@@ -0,0 +1,67 @@
+//go:build integration
+
+package expenses
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateExpenseIntegration(t *testing.T) {
+	body := bytes.NewBufferString(`{
+		"title": "strawberry smoothie",
+		"amount": 79,
+		"note": "night market promotion discount 10 bath", 
+		"tags": ["food", "beverage"]
+	}`)
+	var exp Expenses
+	res := request(http.MethodPost, uri("expenses"), body)
+	err := res.Decode(&exp)
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusCreated, res.StatusCode)
+	assert.NotEqual(t, 0, exp.ID)
+	assert.Equal(t, "strawberry smoothie", exp.Title)
+	assert.Equal(t, 79.00, exp.Amount)
+	assert.Equal(t, "night market promotion discount 10 bath", exp.Note)
+	assert.Equal(t, []string{"food", "beverage"}, exp.Tags)
+}
+
+func uri(paths ...string) string {
+	host := fmt.Sprint("http://localhost", os.Getenv("PORT"))
+	if paths == nil {
+		return host
+	}
+
+	url := append([]string{host}, paths...)
+	return strings.Join(url, "/")
+}
+
+type Response struct {
+	*http.Response
+	err error
+}
+
+func (r *Response) Decode(v interface{}) error {
+	if r.err != nil {
+		return r.err
+	}
+
+	return json.NewDecoder(r.Body).Decode(v)
+}
+
+func request(method, url string, body io.Reader) *Response {
+	req, _ := http.NewRequest(method, url, body)
+	req.Header.Add("Content-Type", "application/json")
+	client := http.Client{}
+	res, err := client.Do(req)
+	return &Response{res, err}
+}
