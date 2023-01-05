85c595d57796e417cbf24402fbaa78bf048409a7
Thepaeth <theera.cpe12@gmail.com>
2023-01-05 17:23:35 +0700

    add mock db

--- pkg/expenses/expenses.go
+++ pkg/expenses/expenses.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/lib/pq"
 )
 
 type Expenses struct {
@@ -19,20 +20,15 @@ type Err struct {
 }
 
 func CreateExpense(c echo.Context) error {
-	// var exp Expenses
-	// err := c.Bind(&exp)
-	// if err != nil {
-	// 	return c.JSON(http.StatusBadRequest, Err{Message: "Bad Request!!!"})
-	// }
-	// row := db.QueryRow("INSERT INTO expenses (title, amount, note, tags) values($1, $2, $3, $4) RETURNING id", exp.Title, exp.Amount, exp.Note, pq.Array(exp.Tags))
-	// err = row.Scan(&exp.ID)
-	// if err != nil {
-	// 	return c.JSON(http.StatusInternalServerError, Err{Message: "Can't Create Expense!!!!"})
-	// }
-	// return c.JSON(http.StatusCreated, exp)
-	exp := new(Expenses)
-	if err := c.Bind(exp); err != nil {
-		return c.JSON(http.StatusBadRequest, Err{Message: "errror"})
+	var exp Expenses
+	err := c.Bind(&exp)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, Err{Message: "Bad Request!!!"})
+	}
+	row := db.QueryRow("INSERT INTO expenses (title, amount, note, tags) values($1, $2, $3, $4) RETURNING id", exp.Title, exp.Amount, exp.Note, pq.Array(&exp.Tags))
+	err = row.Scan(&exp.ID)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "Can't Create Expense!!!!"})
 	}
 	return c.JSON(http.StatusCreated, exp)
 }
--- pkg/expenses/expenses_test.go
+++ pkg/expenses/expenses_test.go
@@ -5,26 +5,51 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"regexp"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/labstack/echo/v4"
+	"github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
 )
 
-var bodyExpense = `{
+var (
+	bodyExpense = `{
 		"title": "strawberry smoothie",
 		"amount": 79,
 		"note": "night market promotion discount 10 bath", 
 		"tags": ["food", "beverage"]
 	}`
+	mockData = Expenses{
+		Title:  "strawberry smoothie",
+		Amount: 79.00,
+		Note:   "night market promotion discount 10 bath",
+		Tags:   []string{"food", "beverage"},
+	}
+)
 
 func TestCreateExpenseMock(t *testing.T) {
+	// db mock
+	mockdb, mock, _ := sqlmock.New()
+	expMockSql := "INSERT INTO expenses (title, amount, note, tags) values($1, $2, $3, $4) RETURNING id"
+	expMockRow := sqlmock.NewRows([]string{"id"}).AddRow(1)
+
+	mock.ExpectQuery(regexp.QuoteMeta(expMockSql)).WithArgs(mockData.Title, mockData.Amount, mockData.Note, pq.Array(&mockData.Tags)).
+		WillReturnRows(expMockRow)
+
 	// setup echo server
+	body, err := json.Marshal(mockData)
+	if err != nil {
+		t.Error(err)
+		return
+	}
 	e := echo.New()
-	req := httptest.NewRequest(http.MethodPost, "/expenses", bytes.NewBufferString(bodyExpense))
+	req := httptest.NewRequest(http.MethodPost, "/expenses", bytes.NewBufferString(string(body)))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	res := httptest.NewRecorder()
 	c := e.NewContext(req, res)
+	db = mockdb
 
 	// Assertions
 	if assert.NoError(t, CreateExpense(c)) {
@@ -32,7 +57,7 @@ func TestCreateExpenseMock(t *testing.T) {
 		err := json.Unmarshal(res.Body.Bytes(), exp)
 		assert.Nil(t, err)
 		assert.Equal(t, http.StatusCreated, res.Code)
-		assert.Equal(t, 0, exp.ID)
+		assert.Equal(t, 1, exp.ID)
 		assert.Equal(t, "strawberry smoothie", exp.Title)
 		assert.Equal(t, 79.00, exp.Amount)
 		assert.Equal(t, "night market promotion discount 10 bath", exp.Note)
