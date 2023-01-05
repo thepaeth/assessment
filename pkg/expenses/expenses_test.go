2cc3ab034d466bc071b7ace0d82049f1cf1effb0
Thepaeth <theera.cpe12@gmail.com>
2023-01-05 16:10:30 +0700

    back to the basic add unit test ^^

--- pkg/expenses/expenses_test.go
+++ pkg/expenses/expenses_test.go
@@ -3,63 +3,39 @@ package expenses
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
-	"io"
 	"net/http"
-	"os"
-	"strings"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateExpense(t *testing.T) {
-	body := bytes.NewBufferString(`{
+var bodyExpense = `{
 		"title": "strawberry smoothie",
 		"amount": 79,
 		"note": "night market promotion discount 10 bath", 
 		"tags": ["food", "beverage"]
-	}`)
-	var exp Expenses
-	res := request(http.MethodPost, uri("expenses"), body)
-	err := res.Decode(&exp)
+	}`
 
-	assert.Nil(t, err)
-	assert.Equal(t, http.StatusCreated, res.StatusCode)
-	assert.NotEqual(t, 0, exp.ID)
-	assert.Equal(t, "strawberry smoothie", exp.Title)
-	assert.Equal(t, 79.00, exp.Amount)
-	assert.Equal(t, "night market promotion discount 10 bath", exp.Note)
-	assert.Equal(t, []string{"food", "beverage"}, exp.Tags)
-}
+func TestCreateExpenseMock(t *testing.T) {
+	// setup echo server
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/expenses", bytes.NewBufferString(bodyExpense))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	res := httptest.NewRecorder()
+	c := e.NewContext(req, res)
 
-func uri(paths ...string) string {
-	host := fmt.Sprint("http://localhost", os.Getenv("PORT"))
-	if paths == nil {
-		return host
+	// Assertions
+	if assert.NoError(t, CreateExpense(c)) {
+		exp := &Expenses{}
+		err := json.Unmarshal(res.Body.Bytes(), exp)
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusCreated, res.Code)
+		assert.Equal(t, 0, exp.ID)
+		assert.Equal(t, "strawberry smoothie", exp.Title)
+		assert.Equal(t, 79.00, exp.Amount)
+		assert.Equal(t, "night market promotion discount 10 bath", exp.Note)
+		assert.Equal(t, []string{"food", "beverage"}, exp.Tags)
 	}
-
-	url := append([]string{host}, paths...)
-	return strings.Join(url, "/")
-}
-
-type Response struct {
-	*http.Response
-	err error
-}
-
-func (r *Response) Decode(v interface{}) error {
-	if r.err != nil {
-		return r.err
-	}
-
-	return json.NewDecoder(r.Body).Decode(v)
-}
-
-func request(method, url string, body io.Reader) *Response {
-	req, _ := http.NewRequest(method, url, body)
-	req.Header.Add("Content-Type", "application/json")
-	client := http.Client{}
-	res, err := client.Do(req)
-	return &Response{res, err}
 }
